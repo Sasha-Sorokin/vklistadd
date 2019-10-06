bbaa5b38e25b5199959b6dd4419e0f37432c805d
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2019-10-06 22:31:24 +0700

    feat: add support url

--- vklistadd.user.js
+++ vklistadd.user.js
@@ -3,6 +3,7 @@
 // @description Implements a darky button to add communities to feed lists without subscribing to them
 // @version 1.0.0
 // @author Sasha_Sorokin
+// @supportURL https://github.com/Sasha-Sorokin/vkaintegra/issues
 // @run-at document-start
 // @include https://vk.com/*
 // @noframes
